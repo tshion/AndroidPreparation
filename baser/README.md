a7ff7bca571fe55c081587476f6ddc1b68d28180
shion <tenta_shion@yahoo.co.jp>
2020-01-15 00:41:06 +0900

    Timber の整備

--- baser/README.md
+++ baser/README.md
@@ -1,5 +1,11 @@
 # AndroidPreparation.Baser
 Android 開発でよく使う骨組みの整備
+下記の機能が使えるようになります
+
+組込方法 | 機能 | 概要
+--- | --- | ---
+手動 | Timber ヘルパー | Timber 設定手続きの簡略化
 
 
 ## 参考資料
+* [JakeWharton/timber](https://github.com/JakeWharton/timber)
--- baser/src/main/java/work/shion/androidpreparation/baser/MainApplication.kt
+++ baser/src/main/java/work/shion/androidpreparation/baser/MainApplication.kt
@@ -2,4 +2,13 @@ package work.shion.androidpreparation.baser
 
 import android.app.Application
 
-open class MainApplication : Application()
+open class MainApplication : Application(),
+        ITimberAttacher {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Timber の設定
+        setupTimber()
+    }
+}
