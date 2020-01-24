0aaf77f2e0daf08d1ae976b052bdce149f0593aa
shion <tenta_shion@yahoo.co.jp>
2020-01-24 23:22:35 +0900

    実装のブラッシュアップ

--- intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/Product.kt
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/Product.kt
@@ -6,52 +6,29 @@ import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 
-abstract class ExecutableIntent : Intent() {
+abstract class ExecutableIntent : Intent()
 
-    /**
-     * 起動できるかどうか
-     */
-    fun canLaunch(packageManager: PackageManager) = resolveActivity(packageManager) != null
-}
+/** 起動できるかどうか */
+fun ExecutableIntent.canLaunch(packageManager: PackageManager) = resolveActivity(packageManager) != null
 
 
 /**
  * 実行結果を受け取れないIntent
  */
-class ConsumerIntent : ExecutableIntent() {
+class ConsumerIntent : ExecutableIntent()
 
-    /**
-     * @throws Exception
-     */
-    fun launch(activity: Activity) {
-        activity.startActivity(this)
-    }
+/** @throws Exception */
+fun ConsumerIntent.launch(activity: Activity) = activity.startActivity(this)
 
-    /**
-     * @throws Exception
-     */
-    fun launch(fragment: Fragment) {
-        fragment.startActivity(this)
-    }
-}
+/** @throws Exception */
+fun ConsumerIntent.launch(fragment: Fragment) = fragment.startActivity(this)
 
 
-/**
- * 実行結果を受け取ることができるIntent
- */
-class SupplierIntent : ExecutableIntent() {
+/** 実行結果を受け取ることができるIntent */
+class SupplierIntent : ExecutableIntent()
 
-    /**
-     * @throws Exception
-     */
-    fun launch(activity: Activity, requestCode: Int) {
-        activity.startActivityForResult(this, requestCode)
-    }
+/** @throws Exception */
+fun SupplierIntent.launch(activity: Activity, requestCode: Int) = activity.startActivityForResult(this, requestCode)
 
-    /**
-     * @throws Exception
-     */
-    fun launch(fragment: Fragment, requestCode: Int) {
-        fragment.startActivityForResult(this, requestCode)
-    }
-}
+/** @throws Exception */
+fun SupplierIntent.launch(fragment: Fragment, requestCode: Int) = fragment.startActivityForResult(this, requestCode)
