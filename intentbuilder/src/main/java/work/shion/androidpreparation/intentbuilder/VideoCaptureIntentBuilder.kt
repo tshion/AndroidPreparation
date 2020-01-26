fe74347c0be731375183c0e4c614a18705bbdde5
shion <tenta_shion@yahoo.co.jp>
2020-01-26 22:03:30 +0900

    動画撮影設定のビルダー追加

---
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/VideoCaptureIntentBuilder.kt
@@ -0,0 +1,39 @@
+package work.shion.androidpreparation.intentbuilder
+
+import android.net.Uri
+import android.provider.MediaStore
+
+/**
+ * 動画を撮影して返す設定ビルダー
+ *
+ * ### 実装例
+ * ``` kotlin
+ * VideoCaptureIntentBuilder().apply {
+ *     setDestination(uri)
+ * }.build()?.launch(activity!!)
+ * ```
+ *
+ * ### 参考文献
+ * [一般的なインテント | Android デベロッパー](https://developer.android.com/guide/components/intents-common#ImageCapture)
+ */
+class VideoCaptureIntentBuilder : IntentBuilder<SupplierIntent>() {
+
+    private var uri: Uri? = null
+
+
+    fun setDestination(uri: Uri) {
+        this.uri = uri
+    }
+
+
+    /**
+     * 与えられた設定からIntent を生成する
+     */
+    override fun build(): SupplierIntent? {
+        val intent = SupplierIntent().apply {
+            action = MediaStore.ACTION_VIDEO_CAPTURE
+        }
+        uri?.also { intent.putExtra(MediaStore.EXTRA_OUTPUT, it) }
+        return intent
+    }
+}
