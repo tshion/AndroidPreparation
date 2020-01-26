8c430f3a1e4bfd6d21ff4f32932567cfde1afd5e
shion <tenta_shion@yahoo.co.jp>
2020-01-26 22:15:03 +0900

    動画モードでカメラアプリ起動のビルダー追加

---
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/CameraMovieIntentBuilder.kt
@@ -0,0 +1,27 @@
+package work.shion.androidpreparation.intentbuilder
+
+import android.provider.MediaStore
+
+/**
+ * 動画モードでカメラアプリ起動の設定ビルダー
+ *
+ * ### 実装例
+ * ``` kotlin
+ * CameraMovieIntentBuilder().apply {
+ * }.build()?.launch(activity!!)
+ * ```
+ *
+ * ### 参考文献
+ * [一般的なインテント | Android デベロッパー](https://developer.android.com/guide/components/intents-common?hl=ja#CameraVideo)
+ */
+class CameraMovieIntentBuilder : IntentBuilder<SupplierIntent>() {
+
+    /**
+     * 与えられた設定からIntent を生成する
+     */
+    override fun build(): SupplierIntent? {
+        return SupplierIntent().apply {
+            action = MediaStore.INTENT_ACTION_VIDEO_CAMERA
+        }
+    }
+}
