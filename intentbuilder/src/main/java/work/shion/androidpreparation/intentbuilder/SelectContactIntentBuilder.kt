6dcca1a212fecefeaa9955262e715a98ce58626d
shion <tenta_shion@yahoo.co.jp>
2020-01-26 22:52:59 +0900

    連絡先選択の設定ビルダーの追記

--- intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/SelectContactIntentBuilder.kt
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/SelectContactIntentBuilder.kt
@@ -9,6 +9,7 @@ import android.provider.ContactsContract
  * ### 実装例
  * ``` kotlin
  * SelectContactIntentBuilder().apply {
+ *     setContentType()
  * }.build()?.launch(activity!!, REQUEST_CODE)
  * ```
  *
@@ -17,13 +18,47 @@ import android.provider.ContactsContract
  */
 class SelectContactIntentBuilder : IntentBuilder<SupplierIntent>() {
 
+    private var mime: String? = null
+
+
+    /**
+     * 連絡先から住所を取得します。
+     */
+    fun setAddress() {
+        mime = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_TYPE
+    }
+
+    /**
+     * 連絡先を選択する
+     */
+    fun setContentType() {
+        mime = ContactsContract.Contacts.CONTENT_TYPE
+    }
+
+    /**
+     * 連絡先からメールアドレスを取得します。
+     */
+    fun setEmail() {
+        mime = ContactsContract.CommonDataKinds.Email.CONTENT_TYPE
+    }
+
+    /**
+     * 連絡先から電話番号を取得します。
+     */
+    fun setPhone() {
+        mime = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+    }
+
+
     /**
      * 与えられた設定からIntent を生成する
      */
     override fun build(): SupplierIntent? {
-        return SupplierIntent().apply {
-            action = Intent.ACTION_PICK
-            type = ContactsContract.Contacts.CONTENT_TYPE
+        return mime?.let {
+            SupplierIntent().apply {
+                action = Intent.ACTION_PICK
+                type = mime
+            }
         }
     }
 }
