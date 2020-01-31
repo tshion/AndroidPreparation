f48b1d8fc548025578b5270a527b4afd39bc8ab9
shion <tenta_shion@yahoo.co.jp>
2020-02-01 00:14:38 +0900

    #1 実行処理のブラッシュアップ

--- intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/Product.kt
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/Product.kt
@@ -2,33 +2,73 @@ package work.shion.androidpreparation.intentbuilder
 
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 
 abstract class ExecutableIntent : Intent()
 
-/** 起動できるかどうか */
-fun ExecutableIntent.canLaunch(packageManager: PackageManager) = resolveActivity(packageManager) != null
-
 
 /**
- * 実行結果を受け取れないIntent
+ * Intent(実行結果なし)
  */
 class ConsumerIntent : ExecutableIntent()
 
-/** @throws Exception */
-fun ConsumerIntent.launch(activity: Activity) = activity.startActivity(this)
+/**
+ * Intent の実行
+ * @param activity 起動元
+ * @param onNotFound 起動先が見つからない場合
+ */
+fun ConsumerIntent.launch(activity: Activity, onNotFound: (() -> Unit)? = null) {
+    if (resolveActivity(activity.packageManager) != null) {
+        activity.startActivity(this)
+    } else {
+        onNotFound?.invoke()
+    }
+}
 
-/** @throws Exception */
-fun ConsumerIntent.launch(fragment: Fragment) = fragment.startActivity(this)
+/**
+ * Intent の実行
+ * @param  fragment 起動元
+ * @param onNotFound 起動先が見つからない場合
+ */
+fun ConsumerIntent.launch(fragment: Fragment, onNotFound: (() -> Unit)? = null) {
+    if (fragment.activity?.let { resolveActivity(it.packageManager) } != null) {
+        fragment.startActivity(this)
+    } else {
+        onNotFound?.invoke()
+    }
+}
 
 
-/** 実行結果を受け取ることができるIntent */
+/**
+ * Intent(実行結果あり)
+ */
 class SupplierIntent : ExecutableIntent()
 
-/** @throws Exception */
-fun SupplierIntent.launch(activity: Activity, requestCode: Int) = activity.startActivityForResult(this, requestCode)
+/**
+ * Intent(実行結果あり) の実行
+ * @param activity 起動元
+ * @param requestCode 起動要求コード
+ * @param onNotFound 起動先が見つからない場合
+ */
+fun SupplierIntent.launch(activity: Activity, requestCode: Int, onNotFound: (() -> Unit)? = null) {
+    if (resolveActivity(activity.packageManager) != null) {
+        activity.startActivityForResult(this, requestCode)
+    } else {
+        onNotFound?.invoke()
+    }
+}
 
-/** @throws Exception */
-fun SupplierIntent.launch(fragment: Fragment, requestCode: Int) = fragment.startActivityForResult(this, requestCode)
+/**
+ * Intent(実行結果あり) の実行
+ * @param fragment 起動元
+ * @param requestCode 起動要求コード
+ * @param onNotFound 起動先が見つからない場合
+ */
+fun SupplierIntent.launch(fragment: Fragment, requestCode: Int, onNotFound: (() -> Unit)? = null) {
+    if (fragment.activity?.let { resolveActivity(it.packageManager) } != null) {
+        fragment.startActivityForResult(this, requestCode)
+    } else {
+        onNotFound?.invoke()
+    }
+}
