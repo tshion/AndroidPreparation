316489eac612361d35cd6170a9f70c2b02d48cf2
shion <tenta_shion@yahoo.co.jp>
2020-02-04 13:31:14 +0900

    #5 テストのベース追加

---
+++ intentbuilder/src/androidTest/java/work/shion/androidpreparation/intentbuilder/CalendarEventIntentBuilderTest.kt
@@ -0,0 +1,42 @@
+package work.shion.androidpreparation.intentbuilder
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.provider.CalendarContract.Events
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CalendarEventIntentBuilderTest {
+
+    @get:Rule
+    val testRule = IntentsTestRule(CalendarEventSample::class.java)
+
+
+    @Before
+    fun setUp() {
+        Intents.intending(IntentMatchers.hasAction(Intent.ACTION_INSERT))
+                .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
+    }
+
+    @Test
+    fun test() {
+        onView(withText(CalendarEventSample.paramEmpty)).perform(click())
+        intended(CoreMatchers.allOf(
+                IntentMatchers.hasAction(Intent.ACTION_INSERT),
+                IntentMatchers.hasData(Events.CONTENT_URI)
+        ))
+    }
+}
---
+++ intentbuilder/src/debug/java/work/shion/androidpreparation/intentbuilder/CalendarEventSample.kt
@@ -0,0 +1,32 @@
+package work.shion.androidpreparation.intentbuilder
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.ViewGroup.LayoutParams
+import android.widget.Button
+import android.widget.LinearLayout
+
+class CalendarEventSample : Activity() {
+
+    companion object {
+        const val paramEmpty = "empty"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val root = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+        }
+
+        root.addView(Button(this).apply {
+            setOnClickListener {
+                CalendarEventIntentBuilder().apply {
+                }.build()?.launch(this@CalendarEventSample)
+            }
+            text = paramEmpty
+        }, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+
+        setContentView(root)
+    }
+}
