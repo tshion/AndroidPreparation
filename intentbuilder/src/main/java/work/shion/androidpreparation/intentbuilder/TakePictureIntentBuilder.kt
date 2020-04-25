4898febff78891127be78d2dfdba02f80fb1b8b1
shion <tenta_shion@yahoo.co.jp>
2020-04-25 12:37:21 +0900

    【IntentBuilder】写真撮影のブラッシュアップ

--- intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/TakePictureIntentBuilder.kt
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/TakePictureIntentBuilder.kt
@@ -2,41 +2,51 @@ package work.shion.androidpreparation.intentbuilder
 
 import android.net.Uri
 import android.provider.MediaStore
+import android.webkit.URLUtil
 import work.shion.androidpreparation.intentbuilder.basis.IntentBuilder
 import work.shion.androidpreparation.intentbuilder.basis.SupplierIntent
 
 /**
- * 写真を撮影して返す設定ビルダー
+ * Capture a picture
  *
- * ### 実装例
+ * ### Example1
  * ``` kotlin
  * TakePictureIntentBuilder().apply {
- *     setDestination(uri)
- * }.build()?.launch(activity!!, REQUEST_CODE)
+ *     outputUri = FileProvider.getUriForFile(context, authority, file)
+ * }.build()?.start(from, REQUEST_CODE)
  * ```
  *
- * ### 参考文献
- * [一般的なインテント | Android デベロッパー](https://developer.android.com/guide/components/intents-common#ImageCapture)
+ * ### Example2
+ * ``` kotlin
+ * TakePictureIntentBuilder()
+ *     .outputUri(FileProvider.getUriForFile(context, authority, file))
+ *     .build()
+ *     ?.start(from, REQUEST_CODE)
+ * ```
+ *
+ * ### References
+ * * [Common Intents | Android Developers](https://developer.android.com/guide/components/intents-common#ImageCapture)
  */
-@Deprecated("In development")
 class TakePictureIntentBuilder : IntentBuilder<SupplierIntent>() {
 
-    private var uri: Uri? = null
+    var outputUri: Uri? = null
+        set(value) {
+            if (URLUtil.isContentUrl(value?.toString())) {
+                field = value
+            }
+        }
 
 
-    fun setDestination(uri: Uri) {
-        this.uri = uri
-    }
+    fun outputUri(input: Uri?) = apply { outputUri = input }
 
 
     /**
-     * 与えられた設定からIntent を生成する
+     * Generate an intent by builder's settings.
      */
-    override fun build(): SupplierIntent? {
-        val intent = SupplierIntent().apply {
+    override fun build() = outputUri?.let { uri ->
+        SupplierIntent().apply {
             action = MediaStore.ACTION_IMAGE_CAPTURE
+            putExtra(MediaStore.EXTRA_OUTPUT, uri)
         }
-        uri?.also { intent.putExtra(MediaStore.EXTRA_OUTPUT, it) }
-        return intent
     }
 }
