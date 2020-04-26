c2c76617b63625f6a034f0025464d8f8617604b5
shion <tenta_shion@yahoo.co.jp>
2020-04-26 16:27:32 +0900

    【IntentBuilder】動画撮影のテスト追加

--- intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/TakeVideoIntentBuilder.kt
+++ intentbuilder/src/main/java/work/shion/androidpreparation/intentbuilder/TakeVideoIntentBuilder.kt
@@ -16,7 +16,7 @@ import work.shion.androidpreparation.intentbuilder.basis.TakeVideoIntent
  * fun launch() {
  *     TakeVideoIntentBuilder()
  *         .build()
- *         ?.start(from, REQUEST_CODE)
+ *         .start(from, REQUEST_CODE)
  * ```
  *
  * ### Example2
@@ -29,7 +29,7 @@ import work.shion.androidpreparation.intentbuilder.basis.TakeVideoIntent
  * fun launch() {
  *     TakeVideoIntentBuilder()
  *         .build()
- *         ?.start(from, REQUEST_CODE)
+ *         .start(from, REQUEST_CODE)
  * ```
  *
  * ### References
